fbafeac31aef77fae2a55a0f7d74b733603b4814
arthurshafikov <arthur.shafikov27@gmail.com>
2022-06-05 06:19:17 +0400

    add app initialization

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,4 +1,38 @@
 package app
 
+import (
+	"context"
+	"os/signal"
+	"syscall"
+
+	"github.com/arthurshafikov/events-collector/internal/config"
+	"github.com/arthurshafikov/events-collector/internal/logger"
+	"github.com/arthurshafikov/events-collector/internal/repository"
+	"github.com/arthurshafikov/events-collector/internal/services"
+	"github.com/arthurshafikov/events-collector/pkg/clickhousedb"
+)
+
 func Run() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	defer cancel()
+
+	config := config.NewConfig()
+	logger := logger.NewLogger()
+
+	conn, err := clickhousedb.NewClickhouseDB(ctx, clickhousedb.Options{
+		Address:  config.DB.Address,
+		Database: config.DB.Database,
+		Username: config.DB.Username,
+		Password: config.DB.Password,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	repo := repository.NewRepository(conn)
+	_ = services.NewServices(services.Deps{
+		Context:    ctx,
+		Repository: repo,
+		Logger:     logger,
+	})
 }
