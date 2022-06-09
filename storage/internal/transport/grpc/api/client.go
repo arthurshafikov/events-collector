1ad3f2296643cbf4d8f9e6dce9a230b280d729a6
arthurshafikov <arthur.shafikov27@gmail.com>
2022-06-09 20:24:53 +0400

    add go.work

---
+++ storage/internal/transport/grpc/api/client.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"context"
+	"log"
+
+	"github.com/arthurshafikov/events-collector/internal/transport/grpc/generated"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewGRPCClient(ctx context.Context, address string) generated.CollectorClient {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		<-ctx.Done()
+		conn.Close()
+	}()
+
+	return generated.NewCollectorClient(conn)
+}
