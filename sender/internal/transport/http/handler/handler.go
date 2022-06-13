3ce198c5cfacc259bf983726816efc87147de223
arthurshafikov <arthur.shafikov27@gmail.com>
2022-06-13 07:18:49 +0400

    sender remove useless functions in handler

--- sender/internal/transport/http/handler/handler.go
+++ sender/internal/transport/http/handler/handler.go
@@ -2,16 +2,9 @@ package handler
 
 import (
 	"context"
-	"encoding/json"
 
 	"github.com/arthurshafikov/events-collector/sender/internal/services"
 	"github.com/fasthttp/router"
-	"github.com/valyala/fasthttp"
-)
-
-var (
-	strContentType     = []byte("Content-Type")
-	strApplicationJSON = []byte("application/json")
 )
 
 type Handler struct {
@@ -31,14 +24,4 @@ func NewHandler(
 
 func (h *Handler) Init(r *router.Router) {
 	h.initAuthRoutes(r)
-}
-
-func (h *Handler) setJSONResponse(ctx *fasthttp.RequestCtx, body interface{}) {
-	bodyJSON, err := json.Marshal(body)
-	if err != nil {
-		ctx.Error(err.Error(), 500)
-		return
-	}
-	ctx.SetBody(bodyJSON)
-	ctx.Response.Header.SetCanonical(strContentType, strApplicationJSON)
 }
